f63cc7c2614ec07958949596172cabfd7a0ad7ac
Caleb Hash <cahash@ncsu.edu>
2025-02-25 01:44:56 -0500

    added rectilinear grid bc file

---
+++ src/BC/rectilinearBC.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file rectilinearBC.cpp
+ * @brief rectilinear BC logic
+ * @author Caleb Hash
+ * @date 2025-02-25
+ */
+
+#include <matar.h>
+
+/**
+ * @brief BC logic to set the no-slip wall conditions
+ */
+void NoSlipWall() {
+
+  // set the velocity conditions
+  
+  // set the pressure condition
+}
